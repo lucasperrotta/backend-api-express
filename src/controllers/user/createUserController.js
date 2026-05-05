43ceb3808f1ad3315b91fac89fdbfffaa8622a6d
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2026-05-05 19:38:46 -0300

    Melhora a validação de usuário no controlador de criação e deleção, adicionando mensagens de erro personalizadas e utilizando Zod para validação no modelo userModel.js.

--- src/controllers/user/createUserController.js
+++ src/controllers/user/createUserController.js
@@ -1,14 +1,19 @@
 import { createUser, validateUser } from "../../models/userModel.js"
+import { flattenError, treeifyError } from "zod"
 
 export async function createUsersController(req, res) {
   const user = req.body
 
   const { success, error, data } = validateUser(user, { id: true })
 
+  console.log(flattenError(error)) //funciona pra um nivel de erro, ou seja, quando o erro é apenas em um campo
+  console.log(treeifyError(error)) //funciona para mais de um nivel, ex: endereço.rua, endereço.numero, etc
+
   if (!success) {
     return res.status(400).json({
       message: "Erro de validação",
-      fieldErrors: error.flatten().fieldErrors,
+      fieldErrors: flattenError(error).fieldErrors
+      //fieldErrors: error.flatten().fieldErrors,
     })
   }
 
