46e4efd4b9e7f3736a70e3a19efac589c8b91cbb
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2026-05-05 19:20:01 -0300

    Adiciona validação de usuário utilizando Zod no modelo userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,11 +1,26 @@
 import { prisma } from "../helpers/dbConnection.js"
-
+import * as z from "zod"
 // user = {
 //   name: "Lucas Perrotta Barbosa",
 //   email: "perrotta.lucas@gmail.com",
 //   pass: "12345678",
 //   avatar: "https://github.com/lucasperrotta.png",
 // }
+
+const userSchema = z.object({
+  id: z.int().positive(),
+  avatar: z.string().url().max(500),
+  name: z.string().min(3).max(255),
+  email: z.string().email(),
+  pass: z.string().min(6).max(255),
+})
+
+export const validateUser = (user, partial = false) => {
+  if (partial) {
+    return userSchema.partial(partial).safeParse(user)
+  }
+  return userSchema.safeParse(user)
+}
 
 export const createUser = async (user) => {
   return await prisma.user.create({
