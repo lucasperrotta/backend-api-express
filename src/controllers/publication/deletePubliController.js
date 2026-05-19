9507d671ff09707b5f39a636fd5cf729965afa8e
Lucas Perrotta Barbosa <perrotta.lucas@gmail.com>
2026-05-19 17:04:47 -0300

    feat: adiciona tratamento de erro e validação aprimorada nos controladores de publicação

--- src/controllers/publication/deletePubliController.js
+++ src/controllers/publication/deletePubliController.js
@@ -3,25 +3,35 @@ import {
   validatePublication,
 } from "../../models/publicationModel.js"
 
-export async function deletePubliController(req, res) {
-  const id = req.params.id
+export async function deletePubliController(req, res, next) {
+  try {
+    const id = req.params.id
 
-  const { success, error, data } = validatePublication(
-    { id: +id },
-    { title: true, description: true, author: true },
-  )
+    const { success, error, data } = validatePublication(
+      { id: +id },
+      { title: true, description: true, author: true },
+    )
 
-  if (!success) {
-    return res.status(400).json({
-      message: "Erro de validação",
-      fieldErrors: error,
+    if (!success) {
+      return res.status(400).json({
+        message: "Erro de validação",
+        fieldErrors: error,
+      })
+    }
+
+    const result = await deletePublication(data.id)
+
+    return res.json({
+      message: "Publicação deletada com sucesso!",
+      publication: result,
     })
+  } catch (error) {
+    if (error.code === "P2025") {
+      console.log(error.message)
+      return res.status(404).json({
+        message: "Publicação não encontrada para ser deletada.",
+      })
+    }
+    next(error)
   }
-
-  const result = await deletePublication(data.id)
-
-  return res.json({
-    message: "Publicação deletada com sucesso!",
-    publication: result,
-  })
 }
--- src/controllers/publication/updatePubliController.js
+++ src/controllers/publication/updatePubliController.js
@@ -1,3 +1,38 @@
-export function updatePubliController(req, res) {
-  res.send("Exemplo de PUT na rota /publication respondida pelo Controller")
+export async function updatePubliController(req, res, next) {
+  try {
+    const { id } = req.params
+    const publication = req.body
+    publication.id = +id
+
+    const {
+      success,
+      error,
+      data: publicationValidated,
+    } = validatePublication(publication)
+
+    if (!success) {
+      return res.status(400).json({
+        message: "Erro de validação",
+        fieldErrors: error,
+      })
+    }
+
+    const result = await updatePublication(
+      publicationValidated,
+      publicationValidated.id,
+    )
+
+    return res.json({
+      message: "Publicação atualizada com sucesso!",
+      publication: result,
+    })
+  } catch (error) {
+    if (error.code === "P2025") {
+      console.log(error.message)
+      return res.status(404).json({
+        message: "Publicação não encontrada para ser atualizada.",
+      })
+    }
+    next(error)
+  }
 }
